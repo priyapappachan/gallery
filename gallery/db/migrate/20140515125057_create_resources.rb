a7bd44a748a80c3286246f44616eb3f18c402236
dexter <dexter@lab.(none)>
2014-05-20 21:17:02 +0530

    simple blog application

--- gallery/db/migrate/20140515125057_create_resources.rb
+++
@@ -1,8 +0,0 @@
-class CreateResources < ActiveRecord::Migration
-  def change
-    create_table :resources do |t|
-     # t.string :tag_list
-      t.timestamps
-    end
-  end
-end
--- gallery/db/migrate/20140516094245_create_tags.rb
+++
@@ -1,9 +0,0 @@
-class CreateTags < ActiveRecord::Migration
-  def change
-    create_table :tags do |t|
-      t.string :name
-      #t.references :resource, index: true	
-      t.timestamps
-    end
-  end
-end
