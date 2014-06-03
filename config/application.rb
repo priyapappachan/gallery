93528695982d45a38cf7bbf0ab0234ab6cb2bc47
priyapappachan <priyapappachan010@gmail.com>
2014-06-03 20:00:13 +0530

    changes for deploying in heroku

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-config.serve_static_assets = true
+#config.serve_static_assets = true
 #config.assets.initialize_on_precompile = false
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
